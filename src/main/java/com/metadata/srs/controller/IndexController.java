1c4a1f423f40a7776143bc670bec6fc0af55cf81
Hossam Kandel <hossam20052@hotmail.com>
2023-12-11 11:10:47 +0400

    Update IndexController.java

--- src/main/java/com/metadata/srs/controller/IndexController.java
+++ src/main/java/com/metadata/srs/controller/IndexController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+
 @RestController
 @RequestMapping(path = "/")
-
+@Api(value = "Index", description = "Index Page", tags = {"Index"})
 public class IndexController {
 
     @Autowired
